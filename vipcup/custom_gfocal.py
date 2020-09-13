b348a624b28db9d27117475e1c49b164e30e13ac
icipvip <70755110+icipvip@users.noreply.github.com>
2020-09-13 19:28:15 +0600

    Create custom_gfocal.py

---
+++ vipcup/custom_gfocal.py
@@ -0,0 +1,62 @@
+_base_ = '../configs/gfl/gfl_r50_fpn_mstrain_2x_coco.py'
+
+dataset_type = 'CocoDataset'
+classes = ('vehicle',)
+data_root = '../data/'
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train/labels_cocoformat.json',
+        img_prefix=data_root + 'train/images/'
+    ),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/labels_cocoformat.json',
+        img_prefix=data_root + 'test/images/'
+    ),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/labels_cocoformat.json',
+        img_prefix=data_root + 'test/images/'
+    )
+)
+
+model = dict(
+    bbox_head=dict(
+        num_classes=1
+    )
+)
+
+total_epochs = 5
+
+optimizer = dict(
+    lr=0.005
+)
+
+lr_config = dict(
+    step=[3, 4]
+)
+
+test_cfg = dict(
+    score_thr=0.5
+)
+
+log_config = dict(
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ]
+)
+
+checkpoint_config = dict(
+    create_symlink=False
+)
+
+load_from = 'http://download.openmmlab.com/mmdetection/v2.0/gfl/gfl_r50_fpn_mstrain_2x_coco/gfl_r50_fpn_mstrain_2x_coco_20200629_213802-37bb1edc.pth'
+work_dir = '../logdir'
